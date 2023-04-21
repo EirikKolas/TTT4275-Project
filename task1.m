8ee81f1e1591970c0568e1bff4b8c21fa9809126
MarekaGit <122736562+MarekaGit@users.noreply.github.com>
2023-04-21 20:26:04 +0200

    Update task1.m

--- task1.m
+++ task1.m
@@ -1,6 +1,6 @@
 %% Load data
 all_data = load_music_data('Classification music/GenreClassData_30s.txt');
-
+all_data_normalized = data_set_normalizer(all_data);
 
 % Choose features
 features = ["spectral_rolloff_mean", "mfcc_1_mean", "spectral_centroid_mean", "tempo"];
@@ -8,7 +8,7 @@ features = ["spectral_rolloff_mean", "mfcc_1_mean", "spectral_centroid_mean", "t
 % Split data in training-sets and test-sets
 training_set = [];
 test_set = [];
-for data = all_data
+for data = all_data_normalized
     temp.label = data.Genre;
     for i = 1:length(features)
         temp.features(i,1) = data.(features(i));
@@ -32,4 +32,3 @@ classified_data = k_NN_classifier(training_set, test_set, 5);
 diag_sum = sum(diag(confusion));
 total_sum = sum(confusion(:));
 error_rate = (total_sum - diag_sum)/total_sum
-
