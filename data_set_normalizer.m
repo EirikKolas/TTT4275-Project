6b3d80b21111d3606495681d84b8011c0f465bc1
MarekaGit <122736562+MarekaGit@users.noreply.github.com>
2023-04-21 20:23:46 +0200

    Add files via upload

---
+++ data_set_normalizer.m
@@ -0,0 +1,15 @@
+function data_set_normalized = data_set_normalizer(all_data)
+    
+    data_table = struct2table(all_data);
+    fields = fieldnames(all_data);
+
+    for j = 1:length(fields)
+        if ~(strcmp(fields(j), 'TrackID') || strcmp(fields(j), 'File') || strcmp(fields(j), 'GenreID') || strcmp(fields(j), 'Genre') || strcmp(fields(j), 'Type'))
+            field = extractfield(all_data, string(fields(j)));
+            normalized = array2table(normalize(field,'norm')');
+            data_table(:,j) = normalized;
+        end
+    end 
+
+    data_set_normalized = table2struct(data_table)';
+end 
